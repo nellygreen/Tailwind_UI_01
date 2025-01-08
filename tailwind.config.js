3572ec89c725d502f227423047ab13142fc707ed
nellygreen <nellygreen.cg@gmail.com>
2025-01-08 02:26:06 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "340px",
+      ms: "540px",
+      lg: "768px",
+      xl: "1180px"
+    },
     extend: {},
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"]
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px"
+      }
+    }
   },
   plugins: [],
 }
